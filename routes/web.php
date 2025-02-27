8afe32065947d8b8959c9f792835b6a3caf90a07
fakhrichargui <137099014+fakhrichargui@users.noreply.github.com>
2025-02-27 17:27:35 +0100

    Initial commit with Laravel project files

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use App\Http\Controllers\FacebookController;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\HelloMail;
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+Route::get('/auth/facebook', [FacebookController::class, 'facebookpage'])->name('auth.facebook');
+Route::get('/auth/facebook/callback', [FacebookController::class, 'handleFacebookCallback']);
+Route::get('/Slideshow', function () {
+    return Inertia::render('Slideshow');
+})->name('Slideshow');
+
+
